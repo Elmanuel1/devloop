eb7db08c05c0414d1619a8fa7b8eed1375f18824
Oluwatoba Aribo <aribooluwatoba@gmail.com>
2026-02-17 18:57:10 -0800

    TOS-23: Create libs/flyway-jooq-classes module with jOOQ generation
    - New module: libs/flyway-jooq-classes with jOOQ + Flyway + TestContainers
    - TsvectorBinding source for tsvector column binding
    - gradle.properties (moduleVersion=0.1.0) and CHANGELOG.md
    - Wired into settings.gradle and root build.gradle SonarQube modules
    - Follows gradle-deps skill: alias() plugins, version catalog, no hardcoded versions

---
+++ libs/flyway-jooq-classes/CHANGELOG.md
@@ -0,0 +1,15 @@
+# Changelog
+
+All notable changes to this module will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.1.0]
+
+### Added
+- Initial extraction of jOOQ code generation from libs/models into dedicated module
+- TsvectorBinding for PostgreSQL tsvector column type
+- TestContainers + Flyway-based jOOQ class generation pipeline
+- Maven publishing to GitHub Packages
+- CI workflow for build and publish on flyway/schema changes
---
+++ libs/flyway-jooq-classes/src/main/java/com/tosspaper/models/jooq/binding/TsvectorBinding.java
@@ -0,0 +1,78 @@
+package com.tosspaper.models.jooq.binding;
+
+import org.jooq.*;
+import org.jooq.conf.ParamType;
+import org.jooq.impl.DSL;
+
+import java.sql.*;
+
+/**
+ * Custom jOOQ binding for PostgreSQL tsvector columns.
+ * Maps tsvector DB type to Java String for full-text search support.
+ */
+public class TsvectorBinding implements Binding<Object, String> {
+
+    @Override
+    public Converter<Object, String> converter() {
+        return new Converter<>() {
+            @Override
+            public String from(Object databaseObject) {
+                return databaseObject == null ? null : databaseObject.toString();
+            }
+
+            @Override
+            public Object to(String userObject) {
+                return userObject;
+            }
+
+            @Override
+            public Class<Object> fromType() {
+                return Object.class;
+            }
+
+            @Override
+            public Class<String> toType() {
+                return String.class;
+            }
+        };
+    }
+
+    @Override
+    public void sql(BindingSQLContext<String> ctx) throws SQLException {
+        if (ctx.render().paramType() == ParamType.INLINED) {
+            ctx.render().visit(DSL.inline(ctx.convert(converter()).value()));
+        } else {
+            ctx.render().sql(ctx.variable()).sql("::tsvector");
+        }
+    }
+
+    @Override
+    public void register(BindingRegisterContext<String> ctx) throws SQLException {
+        ctx.statement().registerOutParameter(ctx.index(), Types.VARCHAR);
+    }
+
+    @Override
+    public void set(BindingSetStatementContext<String> ctx) throws SQLException {
+        ctx.statement().setString(ctx.index(), ctx.convert(converter()).value());
+    }
+
+    @Override
+    public void set(BindingSetSQLOutputContext<String> ctx) throws SQLException {
+        throw new SQLFeatureNotSupportedException();
+    }
+
+    @Override
+    public void get(BindingGetResultSetContext<String> ctx) throws SQLException {
+        ctx.convert(converter()).value(ctx.resultSet().getString(ctx.index()));
+    }
+
+    @Override
+    public void get(BindingGetStatementContext<String> ctx) throws SQLException {
+        ctx.convert(converter()).value(ctx.statement().getString(ctx.index()));
+    }
+
+    @Override
+    public void get(BindingGetSQLInputContext<String> ctx) throws SQLException {
+        throw new SQLFeatureNotSupportedException();
+    }
+}
